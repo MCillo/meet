971070da2a5325489f1157181b3e78387760b8ce
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-08 07:14:41 -0400

    Update Event.js
    added html elements for event info

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,20 @@
 // Src/components/Event.js
 
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+    const [showDetails, setShowDetails] = useState(false);
     return (
-        <li></li>
+        <li>
+            {/* displays the event name/title */}
+            <h2> {event.summary} </h2>
+            {/* displays the event date */}
+            <h3> {event.start} </h3>
+            {/* displays the event location */}
+            <p> {event.location} </p>
+            {/* button for displaying event details */}
+            <button onClick={showDetails}>Details</button>
+        </li>
     );
 }
 
