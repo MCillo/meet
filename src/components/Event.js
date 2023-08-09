20219c4461d2eb030b214e3ee4f350126bde29bb
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-08-09 06:20:33 -0400

    Update Event.js
    updated ids for display elements

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,19 +3,19 @@
 import { useState } from "react";
 
 const Event = ({ event }) => {
-    const [showDetails, setShowDetails] = useState(false);
-    return (
-        <li>
-            {/* displays the event name/title */}
-            <h2> {event.summary} </h2>
-            {/* displays the event date */}
-            <h3> {event.start} </h3>
-            {/* displays the event location */}
-            <p> {event.location} </p>
-            {/* button for displaying event details */}
-            <button onClick={showDetails}>Details</button>
-        </li>
-    );
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <li>
+      {/* displays the event name/title */}
+      <h2 id="summary"> {event.summary} </h2>
+      {/* displays the event date */}
+      <h3 id="start"> {event.start} </h3>
+      {/* displays the event location */}
+      <p id="location"> {event.location} </p>
+      {/* button for displaying event details */}
+      {/* <button onClick={showDetails}>Details</button> */}
+    </li>
+  );
 }
 
 export default Event;
