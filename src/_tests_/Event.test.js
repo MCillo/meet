1721fbe8aa37ee9eb71b226f83260f7570c85862
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-12 10:34:03 -0400

    Update Event.test.js
    imported screen from testing library
    changed EventComponent.queryByText to
    screen.queryByText

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -1,6 +1,6 @@
 // src/_tests_/EventList.test.js
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Event from "../components/Event";
 import mockData from "../mock-data";
 import { UserEvent } from "@testing-library/user-event";
@@ -13,18 +13,33 @@ describe('<Event />, component', () => {
     EventComponent = render(<Event event={mockEvent} />)
   });
 
+  // test('has the events title', () => {
+  //   const eventTitle = EventComponent.queryByText(mockEvent.summary);
+  //   expect(eventTitle).toBeInTheDocument();
+  // });
+
   test('has the events title', () => {
-    const eventTitle = EventComponent.queryByText(mockEvent.summary);
+    const eventTitle = screen.queryByText(mockEvent.summary);
     expect(eventTitle).toBeInTheDocument();
   });
 
+  // test('has the events location', () => {
+  //   const eventLocation = EventComponent.queryByText(mockEvent.location);
+  //   expect(eventLocation).toBeInTheDocument();
+  // });
+
   test('has the events location', () => {
-    const eventLocation = EventComponent.queryByText(mockEvent.location);
+    const eventLocation = screen.queryByText(mockEvent.location);
     expect(eventLocation).toBeInTheDocument();
   });
 
+  // test('has the event details button', () => {
+  //   const showDetailsButton = EventComponent.queryByText('Show Event Details');
+  //   expect(showDetailsButton).toBeInTheDocument();
+  // });
+
   test('has the event details button', () => {
-    const showDetailsButton = EventComponent.queryByText('Event Details');
+    const showDetailsButton = screen.queryByText('Show Event Details');
     expect(showDetailsButton).toBeInTheDocument();
   });
 
