1c928fee71161401b98c426a588e2b3337c108fe
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-12 10:33:06 -0400

    Update EventList.test.js
    imported screen from testing library
    changed EventListComponent.queryByRole to screen.getByRole

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -1,6 +1,6 @@
 // src/_tests_/EventList.test.js
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import EventList from "../components/EventList";
 import mockData from "../mock-data";
 import { getEvents } from "../api";
@@ -12,13 +12,13 @@ describe('<EventList /> component', () => {
   })
   // Tests whether the app has an element with the role "list"
   test('has an element with "list" role', () => {
-    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+    expect(screen.getByRole("list")).toBeInTheDocument();
   });
 
   // Tests whether the app has elements from mockData from the mock-data.js
   test('renders correct number of events', async () => {
     const allEvents = await getEvents();
     EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+    expect(screen.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
 });
