8cddaaee35178d2360bf7ebabb6df55a3aa22065
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-08-12 08:27:40 -0400

    Update Event.js
    changed display code

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,17 +5,27 @@ import { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li>
-      {/* displays the event name/title */}
-      <h2 id="summary"> {event.summary} </h2>
-      {/* displays the event date */}
-      <h3 id="start"> {event.start} </h3>
-      {/* displays the event location */}
-      <p id="location"> {event.location} </p>
-      {/* button for displaying event details */}
-      {/* <button onClick={showDetails}>Details</button> */}
+    <li className="event">
+      <h2>{event.summary}</h2>
+      <h4>{event.location}</h4>
+
+      <button
+        className="showDetailsButton"
+        onClick={() => {
+          setShowDetails(!showDetails);
+        }}
+      >
+        {showDetails ? 'Hide Event Details' : 'Show Event Details'}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <h3>Event Details</h3>
+          <p>Description: {event.description}</p>
+          <p></p>
+        </div>
+      ) : null}
     </li>
   );
-}
+};
 
 export default Event;
