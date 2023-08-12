0aafcad4ae768978054cb266908721c24779b483
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-12 15:05:58 -0400

    Update Event.test.js
    removed commented code

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -3,7 +3,6 @@
 import { render, screen } from "@testing-library/react";
 import Event from "../components/Event";
 import mockData from "../mock-data";
-import { UserEvent } from "@testing-library/user-event";
 
 const mockEvent = mockData[0];
 
@@ -13,30 +12,15 @@ describe('<Event />, component', () => {
     EventComponent = render(<Event event={mockEvent} />)
   });
 
-  // test('has the events title', () => {
-  //   const eventTitle = EventComponent.queryByText(mockEvent.summary);
-  //   expect(eventTitle).toBeInTheDocument();
-  // });
-
   test('has the events title', () => {
     const eventTitle = screen.queryByText(mockEvent.summary);
     expect(eventTitle).toBeInTheDocument();
   });
 
-  // test('has the events location', () => {
-  //   const eventLocation = EventComponent.queryByText(mockEvent.location);
-  //   expect(eventLocation).toBeInTheDocument();
-  // });
-
   test('has the events location', () => {
     const eventLocation = screen.queryByText(mockEvent.location);
     expect(eventLocation).toBeInTheDocument();
   });
-
-  // test('has the event details button', () => {
-  //   const showDetailsButton = EventComponent.queryByText('Show Event Details');
-  //   expect(showDetailsButton).toBeInTheDocument();
-  // });
 
   test('has the event details button', () => {
     const showDetailsButton = screen.queryByText('Show Event Details');
