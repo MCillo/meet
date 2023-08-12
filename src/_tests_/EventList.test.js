96db49ccf5fe4ebb0b93826d9d66c200defa9926
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-08-12 08:28:04 -0400

    Update EventList.test.js
    removed old code

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -6,26 +6,19 @@ import mockData from "../mock-data";
 import { getEvents } from "../api";
 
 describe('<EventList /> component', () => {
-    let EventListComponent;
-    beforeEach(() => {
-        EventListComponent = render(<EventList />);
-    })
-    // Tests whether the app has an element with the role "list"
-    test('has an element with "list" role', () => {
-        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
-    });
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  })
+  // Tests whether the app has an element with the role "list"
+  test('has an element with "list" role', () => {
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
 
-    // Tests whether the app has exatly 4 elements in the above list
-    // test('renders correct number of events', () => {
-    //     EventListComponent.rerender(<EventList events={
-    //         [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-    //     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
-    // });
-
-    // Tests whether the app has elements from mockData from the mock-data.js
-    test('renders correct number of events', async () => {
-        const allEvents = await getEvents();
-        EventListComponent.rerender(<EventList events={allEvents} />);
-        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
-    });
+  // Tests whether the app has elements from mockData from the mock-data.js
+  test('renders correct number of events', async () => {
+    const allEvents = await getEvents();
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
 });
