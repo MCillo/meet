3eb0a9e3262448dd733e0428af942bb8e1bb4d17
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-08-16 17:32:52 -0400

    Update Event.js
    updated testing code

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,38 @@
 // Src/components/Event.js
 
+// //original code
+// import { useState } from "react";
+
+// const Event = ({ event }) => {
+//   const [showDetails, setShowDetails] = useState(false);
+//   return (
+//     <li className="event">
+//       <h2>{event && event.summary}</h2>
+//       <h4>{event && event.location}</h4>
+
+//       <button
+//         onClick={() => {
+//           setShowDetails(!showDetails);
+//         }}
+//       >
+//         {showDetails ? 'Hide Event Details' : 'Show Event Details'}
+//       </button>
+//       {showDetails ? (
+//         <div className="details">
+//           <h3>Event Details</h3>
+//           <p>Description: {event.description}</p>
+//           <p></p>
+//         </div>
+//       ) : null}
+//     </li>
+//   );
+// };
+
+// export default Event;
+
+
+// test/reading code
+
 import { useState } from "react";
 
 const Event = ({ event }) => {
@@ -7,34 +40,17 @@ const Event = ({ event }) => {
   return (
     <li className="event">
       <h2>{event && event.summary}</h2>
-      <h4>{event && event.location}</h4>
-
-      {/* test code */}
+      <p>{event && event.location}</p>
+      <p>{event && (new Date(event.created)).toUTCString()}</p>
       {showDetails ?
         <p className="details">{event && event.description}</p> :
         null
       }
-      <button className="details" onClick={() => {
+      <button className="details-btn" onClick={() => {
         showDetails ? setShowDetails(false) : setShowDetails(true)
       }}>{showDetails ? "hide details" : "show details"}</button>
-
-      {/* original code */}
-      {/* <button
-        onClick={() => {
-          setShowDetails(!showDetails);
-        }}
-      >
-        {showDetails ? 'Hide Event Details' : 'Show Event Details'}
-      </button>
-      {showDetails ? (
-        <div className="details">
-          <h3>Event Details</h3>
-          <p>Description: {event.description}</p>
-          <p></p>
-        </div>
-      ) : null} */}
     </li>
-  );
-};
+  )
+}
 
 export default Event;
