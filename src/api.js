e6b7a673477dee1acc400ca5983c80895bc2e3b5
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-28 19:59:33 -0400

    Update api.js
    cleaned up code

--- src/api.js
+++ src/api.js
@@ -13,7 +13,6 @@ export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
   return locations;
-  //console.log(locations);
 };
 
 // This function will fetch the list of events
@@ -29,7 +28,7 @@ export const getEvents = async () => {
     removeQuery();
     // const url = "https://tki2mmw5qj.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token; - gives warning = no-useless-concat
     // const url = "https://tki2mmw5qj.execute-api.us-east-1.amazonaws.com/dev/api/get-events/" + token;
-    const url = "https://5q09bhg4hd.execute-api.us-west-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const url = "https://5q09bhg4hd.execute-api.us-west-1.amazonaws.com/dev/api/get-events" + "/" + token; // research where we got this
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
@@ -68,7 +67,7 @@ const checkToken = async (accessToken) => {
   const result = await response.json();
   return result;
 };
-//console.log(error);
+
 // function to remove excess code from URL once finished with it
 const removeQuery = () => {
   let newurl;
@@ -110,127 +109,7 @@ const getToken = async (code) => {
     access_token && localStorage.setItem("access_token", access_token);
     return access_token;
   } catch (error) {
-    console.log(error);
     error.json();
   }
 }
 
-
-// test/reading code
-
-// import mockData from './mock-data';
-
-// /**
-//  *
-//  * @param {*} events:
-//  * The following function should be in the “api.js” file.
-//  * This function takes an events array, then uses map to create a new array with only locations.
-//  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
-//  * The Set will remove all duplicates from the array.
-//  */
-// export const extractLocations = (events) => {
-//   const extractedLocations = events.map((event) => event.location);
-//   const locations = [...new Set(extractedLocations)];
-//   return locations;
-// };
-
-// /**
-//  *
-//  * This function will fetch the list of all events
-//  */
-// export const getEvents = async () => {
-
-//   if (window.location.href.startsWith("http://localhost")) {
-//     return mockData;
-//   }
-
-//   const token = await getAccessToken();
-
-//   if (token) {
-//     removeQuery();
-//     const url = "https://tki2mmw5qj.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
-//     const response = await fetch(url);
-//     const result = await response.json();
-//     if (result) {
-//       return result.events;
-//     } else return null;
-//   }
-// };
-
-
-// export const getAccessToken = async () => {
-//   const accessToken = localStorage.getItem('access_token');
-//   const tokenCheck = accessToken && (await checkToken(accessToken));
-
-//   if (!accessToken || tokenCheck.error) {
-//     await localStorage.removeItem("access_token");
-//     const searchParams = new URLSearchParams(window.location.search);
-//     const code = await searchParams.get("code");
-//     if (!code) {
-//       const response = await fetch(
-//         "https://tki2mmw5qj.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-//       );
-//       const result = await response.json();
-//       const { authUrl } = result;
-//       return (window.location.href = authUrl);
-//     }
-//     return code && getToken(code);
-//   }
-//   return accessToken;
-// };
-
-// const checkToken = async (accessToken) => {
-//   const response = await fetch(
-//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-//   );
-//   const result = await response.json();
-//   return result;
-// };
-// //console.log(error); // Error Testing
-
-// const removeQuery = () => {
-//   let newurl;
-//   if (window.history.pushState && window.location.pathname) {
-//     newurl =
-//       window.location.protocol +
-//       "//" +
-//       window.location.host +
-//       window.location.pathname;
-//     window.history.pushState("", "", newurl);
-//   } else {
-//     newurl = window.location.protocol + "//" + window.location.host;
-//     window.history.pushState("", "", newurl);
-//   }
-// };
-
-// // getToken without try...catch
-
-// // const getToken = async (code) => {
-// //   const encodeCode = encodeURIComponent(code);
-// //   const response = await fetch(
-// //     'YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode
-// //   );
-// //   const { access_token } = await response.json();
-// //   access_token && localStorage.setItem("access_token", access_token);
-
-// //   return access_token;
-// // };
-
-// // getToken with try...catch
-
-// const getToken = async (code) => {
-//   try {
-//     const encodeCode = encodeURIComponent(code);
-
-//     const response = await fetch('https://tki2mmw5qj.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`)
-//     }
-//     const { access_token } = await response.json();
-//     access_token && localStorage.setItem("access_token", access_token);
-//     return access_token;
-//   } catch (error) {
-//     console.log(error); // Error Testing
-//     error.json();
-//   }
-// }
