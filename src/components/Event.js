f8728d9bb9db07df92c4c9d1993dd0fbc8568931
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-28 19:59:02 -0400

    Update Event.js
    Cleaned up code

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,38 +1,5 @@
 // Src/components/Event.js
 
-// //original code
-// import { useState } from "react";
-
-// const Event = ({ event }) => {
-//   const [showDetails, setShowDetails] = useState(false);
-//   return (
-//     <li className="event">
-//       <h2>{event && event.summary}</h2>
-//       <h4>{event && event.location}</h4>
-
-//       <button
-//         onClick={() => {
-//           setShowDetails(!showDetails);
-//         }}
-//       >
-//         {showDetails ? 'Hide Event Details' : 'Show Event Details'}
-//       </button>
-//       {showDetails ? (
-//         <div className="details">
-//           <h3>Event Details</h3>
-//           <p>Description: {event.description}</p>
-//           <p></p>
-//         </div>
-//       ) : null}
-//     </li>
-//   );
-// };
-
-// export default Event;
-
-
-// test/reading code
-
 import { useState } from "react";
 
 const Event = ({ event }) => {
