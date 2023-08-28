6c7c88db02d3f2c816ac36d5bd345c1b1a72ce0a
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-28 19:59:22 -0400

    Update App.js
    cleaned up code
    added number of events functionality

--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
 // src/App.js
 
-// // original code
-
-// import CitySearch from './components/CitySearch';
-// import EventList from './components/EventList';
-// import NumberOfEvents from './components/NumberOfEvents';
-// import { useEffect, useState } from 'react';
-// import { extractLocations, getEvents } from './api';
-
-// import './App.css';
-
-// const App = () => {
-//   const [allLocations, setAllLocations] = useState([]);
-//   const [currentNOE, setCurrentNOE] = useState(32);
-//   const [events, setEvents] = useState([]);
-//   const [currentCity, setCurrentCity] = useState("See all cities");
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [currentCity]);
-
-//   const fetchData = async () => {
-//     const allEvents = await getEvents();
-//     const filteredEvents = currentCity === "See all cities" ?
-//       allEvents :
-//       allEvents.filter(event => event.location === currentCity)
-//     setEvents(filteredEvents.slice(0, currentNOE)); // error with .slice on github pages
-//     setAllLocations(extractLocations(allEvents));
-//   }
-
-//   return (
-//     <div className="App">
-//       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-//       <NumberOfEvents />
-//       <EventList events={events} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// test/reading code
-
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
@@ -58,14 +16,13 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
-  }, [currentCity]);
+  }, [currentCity, currentNOE]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
-    console.log(allEvents, filteredEvents);
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
@@ -73,7 +30,7 @@ const App = () => {
   return (
     <div className="App">
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
       <EventList events={events} />
     </div>
   );
