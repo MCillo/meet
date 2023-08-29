06bc0f859dadaba98c10c62cbf186de7820d6549
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-29 15:16:58 -0400

    Update Event.test.js
    cleaned up unused code

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -1,38 +1,5 @@
 // src/_tests_/EventList.test.js
 
-// // original code
-// import { render, screen } from "@testing-library/react";
-// import Event from "../components/Event";
-// import mockData from "../mock-data";
-
-// const mockEvent = mockData[0];
-
-// // App Unit Testing Code
-// describe('<Event />, component', () => {
-//   let EventComponent;
-//   beforeEach(() => {
-//     EventComponent = render(<Event event={mockEvent} />)
-//   });
-
-//   test('has the events title', () => {
-//     const eventTitle = screen.queryByText(mockEvent.summary);
-//     expect(eventTitle).toBeInTheDocument();
-//   });
-
-//   test('has the events location', () => {
-//     const eventLocation = screen.queryByText(mockEvent.location);
-//     expect(eventLocation).toBeInTheDocument();
-//   });
-
-//   test('has the event details button', () => {
-//     const showDetailsButton = screen.queryByText('Show Event Details');
-//     expect(showDetailsButton).toBeInTheDocument();
-//   });
-
-// })
-
-// test/reading code
-
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { getEvents } from '../api';
@@ -47,18 +14,15 @@ describe('<Event /> component', () => {
   });
 
   test('renders event Title', () => {
-    //expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
-    expect(EventComponent.screen(allEvents[0].summary)).toBeInTheDocument();
+    expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
   });
 
   test('renders event location', () => {
-    //expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
-    expect(EventComponent.screen(allEvents[0].location)).toBeInTheDocument();
+    expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
   });
 
   test('renders event details button with the title (show details)', () => {
-    //expect(EventComponent.queryByText('show details')).toBeInTheDocument();
-    expect(EventComponent.screen('show details')).toBeInTheDocument();
+    expect(EventComponent.queryByText('show details')).toBeInTheDocument();
   });
 
   test("by default, event's details section should be hidden", () => {
