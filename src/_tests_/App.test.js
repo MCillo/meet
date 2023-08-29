3cca385935b497d33f8fc144e15ed92dbefeda6e
MCillo <117599499+MCillo@users.noreply.github.com>
2023-08-28 20:00:07 -0400

    Update App.test.js
    cleaned up code

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -52,7 +52,21 @@ describe('<App /> integration', () => {
     allRenderedEventItems.forEach(event => {
       expect(event.textContent).toContain("Berlin, Germany");
     });
+  });
 
+  // integration test for number of events displayed
+  test('renders number of events based on users selection', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+    const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole('textbox');
+
+    await userEvent.type(NumberOfEventsInput, '{backspace}{backspace}10');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+    expect(allRenderedEventItems.length).toEqual(10);
   });
 
 });
