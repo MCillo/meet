783fcdccb7a796b082d51ea2e8bc240e667f219b
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-09-19 11:38:37 -0400

    Update NumberOfEvents.js
    added Error Alert

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,21 +1,31 @@
 // src/components/NumberOfEvents.js
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
-    const handleChange = (event) => {
-        const value = event.target.value;
-        setCurrentNOE(value);
-    };
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setCurrentNOE(value);
 
-    return (
-        <div id="number-of-events">
-            <input
-                type="text"
-                defaultValue="32"
-                onChange={handleChange}
-                data-testid="numberOfEventsInput"
-            />
-        </div>
-    )
+    let infoText;
+    if (isNaN(value) || value <= 0) {
+      infoText = 'Please enter a positive number.';
+      setErrorAlert(infoText);
+    } else {
+      infoText = '';
+      setErrorAlert(infoText);
+      setCurrentNOE(value);
+    }
+  };
+
+  return (
+    <div id="number-of-events">
+      <input
+        type="text"
+        defaultValue="32"
+        onChange={handleChange}
+        data-testid="numberOfEventsInput"
+      />
+    </div>
+  )
 }
 
 export default NumberOfEvents
