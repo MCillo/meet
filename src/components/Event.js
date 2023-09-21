7a8ce45fe1aad0af598431fa450de0b076f98060
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-09-21 15:43:43 -0400

    Update Event.js
    tested code

--- src/components/Event.js
+++ src/components/Event.js
@@ -10,7 +10,8 @@ const Event = ({ event }) => {
       <p>{event && event.location}</p>
       <p>{event && (new Date(event.created)).toUTCString()}</p>
       {showDetails ?
-        <p className="details">{event && event.description}</p> :
+        <p data-testid='details' className="details">{event && event.description}</p> :
+        //<p data-testid='details' className="details">{event && event.description}</p> :
         null
       }
       <button className="details-btn" onClick={() => {
@@ -21,3 +22,7 @@ const Event = ({ event }) => {
 }
 
 export default Event;
+
+
+
+
