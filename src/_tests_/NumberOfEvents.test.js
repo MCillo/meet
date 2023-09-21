445a7c6a112c0e4919d7376ac29623eb76ebd678
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-09-21 15:43:35 -0400

    Update NumberOfEvents.test.js
    tested code

--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -13,21 +13,21 @@ describe('<NumberOfEvents /> component', () => {
 
   // test that NumberOfEvents component contains element with role textbox
   test('has an element that has textbox role', () => {
-    const numberOfEventsInput = screen.queryAllByRole('textbox');
-    expect(screen.getAllByRole("textbox")).toBeInTheDocument();
+    const numberOfEventsInput = NumberOfEventsComponent.queryByRole('textbox');
+    expect(numberOfEventsInput).toBeInTheDocument();
   });
 
   // test that the default value of number of events listed
   test('that default value for number of events field is 32', () => {
-    const numberOfEventsInput = screen.querAllByRole('textbox');
-    expect(screen.getAllByRole("textbox")).toHaveValue('32');
+    const numberOfEventsInput = NumberOfEventsComponent.queryByRole('textbox');
+    expect(numberOfEventsInput).toHaveValue('32');
   });
 
   // test that the value of NumberOfEvents components textbox value changes as user types
   test('number of events displayed updates as the user types in input textbox', async () => {
-    const numberOfEventsInput = screen.queryAllByRole('textbox');
+    const numberOfEventsInput = NumberOfEventsComponent.queryByRole('textbox');
     await userEvent.type(numberOfEventsInput, '{backspace}{backspace}10');
-    expect(screen.getAllByRole("textbox")).toHaveValue('10');
+    expect(numberOfEventsInput).toHaveValue('10');
   });
 })
 
