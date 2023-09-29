b93a2e8de7757b82394d19a2ba3e47c7ef998847
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-09-28 20:50:55 -0400

    Update showHideAnEventsDetails.test.js
    update

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -2,7 +2,7 @@
 /* eslint-disable testing-library/no-node-access */
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import App from '../App';
-import { render, waitFor, within } from '@testing-library/react';
+import { screen, render, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
@@ -18,7 +18,10 @@ defineFeature(feature, test => {
 
     when('the list of cities is displayed', async () => {
       const AppDOM = AppComponent.container.firstchild;
-      const EventListDOM = AppDOM.querySelector('#event-list');  // ERROR 
+
+
+      const EventListDOM = AppDOM.querySelector('event-list')  // ERROR 
+
 
       await waitFor(() => {
         const EventListItems = within(EventListDOM).queryAllByRole('listitem');
@@ -55,7 +58,7 @@ defineFeature(feature, test => {
     then('the details for the selected event are displayed to the user', () => {
       const eventDom = AppComponent.container.firstChild;
       const details = eventDom.querySelector('.details');
-      expect(details).toBeInTheDocument(); //ERROR
+      expect(details).toBeInTheDocument();
     });
   });
 
@@ -78,7 +81,7 @@ defineFeature(feature, test => {
 
       const eventDom = AppComponent.container.firstChild;
       const details = eventDom.querySelector('.details');
-      expect(details).toBeInTheDocument(); // ERROR
+      expect(details).not.toBeInTheDocument();
     });
 
     when('the user closes the event details view', async () => {
